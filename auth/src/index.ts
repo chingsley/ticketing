d8960bb39e2c208844bfc9b85c0a403162287b7e
chingsley <eneja.kc@gmail.com>
2021-09-20 18:26:15 +0100

    - Setup ingress-ngnix

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,13 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+app.get('/healthcheck', (req, res) => {
+  res.send('Auth service is up and running....')
+});
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there!')
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000...');
 });
