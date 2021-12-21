74f6130f1e23fe00ab6d46a59462abd98b354857
Kingsley Eneja <eneja.kc@gmail.com>
2021-12-21 05:14:20 +0100

    - Implement app header

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,14 +2,33 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '../styles/app.css';
+import buildClient from '../api/build-client';
 
-function App({ Component, pageProps }) {
+import Header from '../components/header';
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <>
+      <Header currentUser={currentUser} />
       <Component {...pageProps} />
       <ToastContainer />
     </>
   );
-}
+};
 
-export default App;
+AppComponent.getInitialProps = async (appContext) => {
+  // console.log(Object.keys(appContext));
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  // console.log(data);
+
+  return { pageProps, ...data };
+};
+
+export default AppComponent;
