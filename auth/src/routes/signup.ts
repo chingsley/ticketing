f7721c566fc871b3783f459056fff617976ad621
Kingsley Eneja <eneja.kc@gmail.com>
2021-12-23 15:03:35 +0100

    - Code reuse across services using npm package

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
-
-import { UniqueFieldViolationError } from '../errors/unique-field-violation-error';
-import { validateRequest } from '../middlewares/validate-request';
+import { validateRequest, UniqueFieldViolationError } from '@chingsley_tickets/common';
 import { User } from '../models/user';
 
 const router = express.Router();
