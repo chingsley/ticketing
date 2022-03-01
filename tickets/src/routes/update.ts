38dc0d8fe21d2602abe5337ba87192be7b2e0f1b
Kingsley Eneja <eneja.kc@gmail.com>
2022-03-01 18:27:07 +0100

    - Publish ticket:updated event when ticket is updated

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -8,6 +8,8 @@ import {
   constants,
 } from '@chingsley_tickets/common';
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const { WRONG_TICKET_OWNER } = constants.errorCodes;
 
@@ -45,6 +47,12 @@ router.put(
 
     ticket.set(req.body);
     await ticket.save();
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    })
     res.send(ticket);
   }
 );
