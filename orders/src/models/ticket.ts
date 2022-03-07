a999b8497b0655026c7723847314f9b4e095d424
Kingsley Eneja <eneja.kc@gmail.com>
2022-03-07 20:58:15 +0100

    - Add new endpont to create new order

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,13 +1,17 @@
 import mongoose from 'mongoose';
+import { Order, OrderStatus } from './order';
+
 
 interface TicketAttrs {
   title: string;
   price: number;
 }
 
+// the instance of a ticket
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  isReserved(): Promise<boolean>
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -25,6 +29,10 @@ const ticketSchema = new mongoose.Schema(
       required: true,
       min: 0,
     },
+    order: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Order'
+    }
   },
   {
     toJSON: {
@@ -39,6 +47,19 @@ const ticketSchema = new mongoose.Schema(
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
 };
+
+ticketSchema.methods.isReserved = async function() { 
+  // isReserved function must be 'function' keyword NOT arrow =>
+  // so that 'this' keyword will refer the the instance on which isReserved is called,
+  // and NOT refer to the Ticket model generatlly
+  const existingOrder  = await Order.findOne({
+    ticket: this, // "this" the ticket instance on which "isReserved" is called. Outside here, this would be replaced by the tccket instance
+    status: { $ne: OrderStatus.Cancelled }
+  });
+
+  return !!existingOrder;
+
+}
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
 
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,59 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import { NotFoundError, requireAuth, validateRequest, BadRequestError, OrderStatus } from '@chingsley_tickets/common';
+import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
+
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+const EXPIRATION_WINDOW_SECONDS = 15 * 60; // this should be treated as a market config
+
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input)) // NOTE: this check creates a sublte coupling with the ticketId. Cause if the structure of the ticketId changes in ticket srvice, then this service will be affectedru
+      .withMessage('A valid mongodb ticketId is required'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError(`No Ticket matches the id of ${ticketId}`)
+    }
+
+    // Make sure that this ticket is not already reserved
+    const isReerved = await ticket.isReserved();
+    if(isReerved) {
+      throw new BadRequestError('Ticket is already reserved')
+    }
+
+    // Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+    await order.save();
+
+    // PUblish an event saying that an order was created
+    // TODO...
+
+    res.status(200).send(order);
+  }
+);
 
 export { router as newOrderRouter };
