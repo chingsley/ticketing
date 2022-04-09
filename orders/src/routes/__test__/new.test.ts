5c8c7fc0f1105f9fb360c846c32500fd1be5db04
Kingsley Eneja <eneja.kc@gmail.com>
2022-04-09 18:33:49 +0100

    - Test order publishers

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
-// import { natsWrapper } from '../../nats-wrapper'; // even though the relative path points to the actual impmementaiton of nats-wrapper, jest will intead import the mock version from '__mock__'
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
 import { Order, OrderStatus } from '../../models/order';
+import { natsWrapper } from '../../nats-wrapper'; // even though the relative path points to the actual impmementaiton of nats-wrapper, jest will intead import the mocked version from '__mock__'
 
 
 it('returns an error if the ticket does not exist', async() => {
@@ -53,4 +53,18 @@ it('reserves a ticket', async() => {
   .expect(201);
 });
 
-it.todo('emits an order created event')
+it('emits an order created event', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  await request(app)
+  .post('/api/orders')
+  .set('Cookie', global.signin())
+  .send({ ticketId: ticket.id })
+  .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+})
